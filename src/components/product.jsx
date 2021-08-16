14efd646ceb29b2d4391e4766d2d6a1c390228a2
user <y.dubovitsky@gmail.com>
2021-08-16 13:59:42 +0300

    added a custom hook that encapsulates the logic and state of the count of products

--- src/components/product.jsx
+++ src/components/product.jsx
@@ -1,8 +1,16 @@
+import UseAmount from "../hooks/useAmount"
+
 export default function product(props) {
+
+  const {amount, decrement, increment} = UseAmount(0);
+
   return (
     <div>
       <p>{props.product.name}</p>
       <p>${props.product.price}</p>
+      <p>Amount: {amount}</p>
+      <button onClick={decrement}>-</button>
+      <button onClick={increment}>+</button>
     </div>
   )
 }
