b9618c6d51cff61157b47914d1bf42a6db182a78
user <y.dubovitsky@gmail.com>
2021-08-19 10:11:55 +0300

    reducers using normalized data now

--- src/redux/reducer/index.js
+++ src/redux/reducer/index.js
@@ -3,10 +3,12 @@ import orderReducer from './orderReducer';
 import restaurantsReducer from './restaurantsReducer';
 import productsReducer from './productsReducer';
 import reviewsReducer from './reviewsReducer';
+import usersReducer from './usersReducer';
 
 export default combineReducers({
   order: orderReducer,
   restaurants: restaurantsReducer,
   products: productsReducer,
-  reviews: reviewsReducer
+  reviews: reviewsReducer,
+  users: usersReducer
 });
--- src/redux/reducer/restaurantsReducer.js
+++ src/redux/reducer/restaurantsReducer.js
@@ -1,4 +1,11 @@
-import { normalizedRestaurants as defaultRestaurant } from '../../fixtures';
+import { normalizedRestaurants } from '../../fixtures';
+
+const defaultRestaurant = normalizedRestaurants.reduce((acc, rest) => (
+  {
+    ...acc,
+    [rest.id]: rest
+  }
+), {});
 
 export default (restaurants = defaultRestaurant, action) => {
   switch (action.type) {
