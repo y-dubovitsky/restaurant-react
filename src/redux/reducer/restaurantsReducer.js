fe0bb255138507b938d0cb40983b8f382327f57e
user <y.dubovitsky@gmail.com>
2021-09-08 12:39:54 +0300

    use immer in reducers.

--- src/redux/reducer/restaurantsReducer.js
+++ src/redux/reducer/restaurantsReducer.js
@@ -1,3 +1,4 @@
+import produce from "immer";
 import { normalizedRestaurants } from '../../fixtures';
 import { ADD_REVIEW } from '../constants/constants';
 
@@ -12,14 +13,24 @@ export default (restaurants = defaultRestaurant, action) => {
   const { type, reviewId, restaurantId } = action;
 
   switch (type) {
+    // case ADD_REVIEW: {
+
+    //   const restaurant = restaurants[restaurantId];
+
+    //   return {
+    //     ...restaurants,
+    //     [restaurantId]: {
+    //       ...restaurant,
+    //       reviews: [...restaurant.reviews, reviewId]
+    //     }
+    //   }
+    // }
     case ADD_REVIEW: {
-      return {
-        ...restaurants,
-        [restaurantId]: {
-          ...restaurants[restaurantId],
-          reviews: [...restaurants[restaurantId].reviews, reviewId]
-        }
-      }
+      return produce(restaurants, draft => {
+        draft[restaurantId].reviews.push(reviewId);
+
+        return draft;
+      })
     }
     default: return restaurants
   }
--- src/redux/reducer/usersReducer.js
+++ src/redux/reducer/usersReducer.js
@@ -1,24 +1,21 @@
+import produce from "immer";
 import { ADD_REVIEW } from '../constants/constants';
 import { normalizedUsers } from '../../fixtures';
 
 const defaultUsers = normalizedUsers.reduce((acc, user) => ({ ...acc, [user.id]: user }), {});
 
-export default (state = defaultUsers, action) => {
+export default produce((draft = defaultUsers, action) => {
 
   const { type, userId, review } = action;
 
   switch (type) {
     case ADD_REVIEW: {
-      console.log(review);
-      return {
-        ...state,
-        [userId]: {
-          id: userId,
-          name: review.name
-        }
+      draft[userId] = {
+        id: userId,
+        name: review.name
       }
+      break;
     }
-    default: return state;
+    default: return draft;
   }
-
-}
+});
