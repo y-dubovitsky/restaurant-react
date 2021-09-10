b17783e257caa28a4049a84a759f5cb7a396aa25
user <y.dubovitsky@gmail.com>
2021-09-10 16:45:45 +0300

    the mock data has been removed. all data is loaded from the server

--- src/index.js
+++ src/index.js
@@ -5,13 +5,9 @@ import './index.css';
 import App from './components/app';
 import store from './redux/store';
 
-import { restaurants } from './fixtures';
-
-window.store = store;
-
 ReactDOM.render(
   <Provider store={store}>
-    <App restaurants={restaurants} />
+    <App restaurants/>
   </Provider>,
   document.getElementById('root')
 );
