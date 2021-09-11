c283c6c93dc42c15d30dc5b21c0498c4580237a5
Yuri <34709153+y-dubovitsky@users.noreply.github.com>
2021-09-11 15:37:12 +0300

    added react-devtools

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,8 @@
 import { applyMiddleware, createStore } from "redux";
+import { composeWithDevTools } from 'redux-devtools-extension';
 import reducer from './reducer';
 import thunk from 'redux-thunk';
 import uuid from './middleware/uuid';
 import api from './middleware/api';
 
-export default createStore(reducer, applyMiddleware(thunk, api, uuid));
+export default createStore(reducer, composeWithDevTools(applyMiddleware(thunk, api, uuid)));
