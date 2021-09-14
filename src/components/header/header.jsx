650d08b055b7c896c3a6d626df0b8305b68d7a77
user <y.dubovitsky@gmail.com>
2021-09-14 15:15:15 +0300

    added logo as img

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,7 +3,9 @@ import style from './header.module.css';
 export default function Header() {
   return (
     <div className={style.header}>
-      <div className={style.logo}>Logo</div>
+      <div className={style.logo}>
+        <img src={process.env.PUBLIC_URL + '/logo.png'} alt="" srcset="" />
+      </div>
       <div className={style.menu}>
         <ul>
           <li>Home</li>
