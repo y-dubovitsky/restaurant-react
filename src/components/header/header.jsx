0c6668e5a033a1d30a4dd11df28a79b4d27efef7
user <y.dubovitsky@gmail.com>
2021-09-21 10:37:02 +0300

    added a link to the card in the header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,8 +1,17 @@
 import CurrencySwitcher from '../currency-switcher/currency-switcher';
 import style from './header.module.css';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingBasket } from '@fortawesome/free-solid-svg-icons';
+import { totalOrderPriceSelector } from '../../redux/features/order';
+import { connect } from 'react-redux';
+import { MoneyContext } from '../../context/money-context';
+import { useContext } from 'react';
 
-export default function Header() {
+function Header({ totalOrderCost }) {
+
+  const { recalculatePrice } = useContext(MoneyContext);
+
   return (
     <div className={style.header}>
       <Link to='/restaurants' className={style.logo}>
@@ -16,8 +25,21 @@ export default function Header() {
           <li>About</li>
           <li>Contacts</li>
           <li>FAQ</li>
+          <li>
+            <NavLink to='/checkout' className={style.checkout}>
+              <i><FontAwesomeIcon icon={faShoppingBasket} />{recalculatePrice(totalOrderCost)}</i>
+            </NavLink>
+          </li>
         </ul>
       </div>
     </div>
   )
 }
+
+const mapStateToProps = (state, props) => {
+  return {
+    totalOrderCost: totalOrderPriceSelector(state)
+  }
+}
+
+export default connect(mapStateToProps)(Header);
